b89dc439996d03e3d36516accbaa63350a316b03
Hedrick Matamorosa <hedrickalbertmatamorosa@gmail.com>
2023-01-03 15:28:21 +0100

    Delete e update funzionanti

--- Esercizio0301/src/main/java/it/corso/esercizio0301/controller/CorsoController.java
+++ Esercizio0301/src/main/java/it/corso/esercizio0301/controller/CorsoController.java
@@ -3,12 +3,14 @@ package it.corso.esercizio0301.controller;
 import it.corso.esercizio0301.model.Corso;
 import it.corso.esercizio0301.repository.CorsoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/corso")
@@ -32,4 +34,18 @@ public class CorsoController {
          Corso cNuovo = corsoRepository.save(c);
         return new ResponseEntity<>(cNuovo, HttpStatus.CREATED);
     }
+
+    @PutMapping ("/update/{id}")
+    public ResponseEntity <Corso> update(@PathVariable("id") Integer id, @RequestBody Corso corsoRequest){
+        Corso c1 = corsoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("TagId " + id + "not found"));
+        c1.setNome(corsoRequest.getNome());
+        return new ResponseEntity<>(corsoRepository.save(c1), HttpStatus.OK);
+    }
+
+    @DeleteMapping ("/delete/{id}")
+    public ResponseEntity <HttpStatus> delete (@PathVariable("id") Integer i){
+        corsoRepository.deleteById(i);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
