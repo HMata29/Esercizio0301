03a9bc758672f5c68177a35dad148527823580c8
Hedrick Matamorosa <hedrickalbertmatamorosa@gmail.com>
2023-01-04 12:24:49 +0100

    messo creazione con associazione, ma unire 2 esistenti da fare

--- Esercizio0301/src/main/java/it/corso/esercizio0301/controller/CorsoController.java
+++ Esercizio0301/src/main/java/it/corso/esercizio0301/controller/CorsoController.java
@@ -1,22 +1,25 @@
 package it.corso.esercizio0301.controller;
 
 import it.corso.esercizio0301.model.Corso;
+import it.corso.esercizio0301.model.Utente;
 import it.corso.esercizio0301.repository.CorsoRepository;
+import it.corso.esercizio0301.repository.UtenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/corso")
 public class CorsoController {
     @Autowired
     CorsoRepository corsoRepository;
+
+    @Autowired
+    UtenteRepository utenteRepository;
 
     @GetMapping("/getAll")
     public ResponseEntity <List <Corso>> getCorsi(){
@@ -52,6 +55,17 @@ public class CorsoController {
     public ResponseEntity <HttpStatus> delete (@PathVariable("id") Integer i){
         corsoRepository.deleteById(i);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/utente/{id}/corso")
+    public ResponseEntity <Corso> creaCorsoUtente(@PathVariable("id") Integer id,@RequestBody  Corso c){
+        Utente utente = utenteRepository.getReferenceById(id);
+        Set<Utente> utenteSet = new HashSet<>();
+        utenteSet.add(utente);
+        c.setUtenti(utenteSet);
+        Corso _corso = corsoRepository.save(c);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
     }
 
 }
--- Esercizio0301/src/main/java/it/corso/esercizio0301/controller/UtenteController.java
+++ Esercizio0301/src/main/java/it/corso/esercizio0301/controller/UtenteController.java
@@ -1,6 +1,8 @@
 package it.corso.esercizio0301.controller;
 
+import it.corso.esercizio0301.model.Corso;
 import it.corso.esercizio0301.model.Utente;
+import it.corso.esercizio0301.repository.CorsoRepository;
 import it.corso.esercizio0301.repository.UtenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
@@ -8,11 +10,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 @RestController
 @RequestMapping("/utente")
 public class UtenteController {
     @Autowired
     UtenteRepository utenteRepository;
+    @Autowired
+    CorsoRepository corsoRepository;
 
     @PostMapping("/insert")
     public ResponseEntity<Utente> inserisci(@RequestBody Utente u){
@@ -34,6 +42,29 @@ public class UtenteController {
     public ResponseEntity <HttpStatus> elimina (@PathVariable("id")Integer id){
         utenteRepository.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PostMapping("/corso/{id}/utente")
+    public ResponseEntity <Corso> creaUtenteCorso(@PathVariable("id") Integer id,@RequestBody  Utente u){
+        Corso corso = corsoRepository.getReferenceById(id);
+        Set<Corso> corsoSet = new HashSet<>();
+        corsoSet.add(corso);
+        u.setCorsi(corsoSet);
+        Utente _utente = utenteRepository.save(u);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
+
+    @PostMapping("/corso/{id_corso}/{id_utente}")
+    public ResponseEntity <HttpStatus> associaCorsoUtente(@PathVariable("id_corso") Integer idCorso, @PathVariable("id_utente")Integer idUtente){
+        Corso c = corsoRepository.findById(idCorso).orElseThrow(() -> new ResourceNotFoundException("CorsoId " + idCorso + "not found"));
+        Utente u = utenteRepository.findById(idUtente).orElseThrow(() -> new ResourceNotFoundException("UtenteId " + idUtente + "not found"));
+
+        c.setUtenti((Set<Utente>) u);
+        u.setCorsi((Set<Corso>) c);
+        Corso _corso = corsoRepository.save(c);
+        Utente _utente = utenteRepository.save(u);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 
